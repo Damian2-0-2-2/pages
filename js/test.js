83be9e077f4df04ce4695159d80726df1c83a08c
Damian677 <walik1.dam@wp.pl>
2023-02-12 17:15:04 +0100

    update
    added, modified files

---
+++ js/test.js
@@ -0,0 +1,54 @@
+if (top.location != location) {
+  top.location.href = location.href;
+}
+
+}
+
+function init() {
+  playBall();
+  if (bookmark) {
+    bookmark();
+  }
+
+}
+var xOff = 5,
+  yOff = 5,
+  xPos = 400,
+  yPos = -100,
+  flagRun = true;
+function newXlt() {
+  xOff = Math.ceil(0 - 6 * Math.random()) * 5 - 10;
+  window.focus();
+}
+function newXrt() {
+  xOff = Math.ceil(7 * Math.random()) * 5 - 10;
+}
+function newYup() {
+  yOff = Math.ceil(0 - 6 * Math.random()) * 5 - 10;
+}
+function newYdn() {
+  yOff = Math.ceil(7 * Math.random()) * 5 - 10;
+}
+function fOff() {
+  flagrun = false;
+}
+function playBall() {
+  xPos += xOff;
+  yPos += yOff;
+  if (xPos > screen.width - 175) {
+    newXlt();
+  }
+  if (xPos < 0) {
+    newXrt();
+  }
+  if (yPos > screen.height - 100) {
+    newYup();
+  }
+  if (yPos < 0) {
+    newYdn();
+  }
+  if (flagRun) {
+    window.moveTo(xPos, yPos);
+    setTimeout(playBall, 50);
+  }
+}
